8bd7e09a1b176d9176adad702e34482aa01fa985
Anthony O <antman@ANTMANS-MacBook-Air.local>
2023-05-31 09:23:30 -0400

    i finished

--- index.js
+++ index.js
@@ -99,7 +99,9 @@ function getUniqueClasses(collection) {
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that are organized by title then by level. The array should only have the title, instructor, and level fields
  */
-function orderClassesByTitleAndLevel(collection) {}
+function orderClassesByTitleAndLevel(collection) {
+  return _.map(_.orderBy(collection, ['title', 'level'], ['asc', 'desc']), ({ title, instructor, level}) => ({title, instructor, level}));
+}
 
 module.exports = {
   numberOfKeys,
