d4088599bd3ab72fd32133d79dd0aba5962df5fb
Anthony O <antman@ANTMANS-MacBook-Air.local>
2023-05-31 09:19:54 -0400

    just getting home from second job

--- index.js
+++ index.js
@@ -89,7 +89,9 @@ function removeInactiveMembers(collection) {
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that have a unique title and a price
  */
-function getUniqueClasses(collection) {}
+function getUniqueClasses(collection) {
+  return _.map(_.uniqBy(collection, 'title'), ({ title, priceInCents}) => ({title, priceInCents}));
+}
 
 /**
  * Get a list of classes organized by title, then by level.
