c4ed68ab74bb993b6a13d98013211280b1df512d
Fig <bethanytrr@gmail.com>
2023-05-30 23:43:41 -0400

    Fourth Test Passed

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ function newMemberArrayToObject(member) {
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  let grouped = _.groupBy(collection, "instructor")
+  return grouped 
+}
 
 /**
  * Remove the age key from the members array of object
